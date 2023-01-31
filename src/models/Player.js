6a15f5982276143f76bb3f78be3b6996195794f1
waikipi <galianez@gmail.com>
2023-01-31 03:39:09 +0100

    amended

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,4 +1,32 @@
-import {Schema, model} from 'mongoose';
+/*import {Schema, model} from 'mongoose';
+import bcrypt from "bcryptjs";
+
+const PlayerSchema = Schema(
+	{
+		user: {type: String, trim: true, unique: true},
+		email: {type: String, trim: true, unique: true},
+		password: { type: String }
+	},
+	{
+		timestamps: true,
+    	versionKey: false
+	}
+);
+
+PlayerSchema.methods.encryptPassword = async (password) => {
+	const salt = await bcrypt.genSalt(10);
+	return await bcrypt.hash(password, salt);
+};
+
+PlayerSchema.methods.matchPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+
+};
+
+export default model('Player', PlayerSchema);*/
+
+import pkg from 'mongoose'
+const {Schema, model} = pkg;
 import bcrypt from "bcryptjs";
 
 const PlayerSchema = Schema(
