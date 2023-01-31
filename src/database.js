32a84e2d7861e24dc25355ea14d4682b4af75660
waikipi <galianez@gmail.com>
2023-01-31 19:19:59 +0100

    mongocluster added

--- src/database.js
+++ src/database.js
@@ -15,7 +15,8 @@ import mongoose from "mongoose";
 // connection to db
 (async () => {
   try {
-    const db = await mongoose.connect('mongodb://localhost:27017/trivial-app');
+    //const db = await mongoose.connect('mongodb://localhost:27017/trivial-app');
+	const db = await mongoose.connect('mongodb+srv://kroskydekellog:FjAbDahO2eGYmmJx@fidmugal.x8gl0h8.mongodb.net/?retryWrites=true&w=majority');
     console.log("Db connected to", db.connection.name);
   } catch (error) {
     console.error(error);
