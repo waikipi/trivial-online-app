a3f13b4ad808cc8d9d3dfc6cf6455d04b6d8f861
waikipi <galianez@gmail.com>
2023-02-01 01:22:43 +0100

    g

--- src/database.js
+++ src/database.js
@@ -13,13 +13,18 @@
 import mongoose from "mongoose"
 
 // connection to db
-(async () => {
+
   try {
     //const db = await mongoose.connect('mongodb://localhost:27017/trivial-app');
-	const db = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/trivial-app');
+	const db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/trivial-app',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		});
     console.log("Db connected to", db.connection.name);
   } catch (error) {
     console.error(error); 
   }
-})();
 
