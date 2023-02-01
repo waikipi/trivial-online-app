ad7cd947f2729cc16b3c27a1dd5953b9b2781c05
waikipi <galianez@gmail.com>
2023-02-01 01:36:26 +0100

    g

--- src/database.js
+++ src/database.js
@@ -16,7 +16,7 @@ import mongoose from "mongoose";
 
   try {
     //const db = await mongoose.connect('mongodb://localhost:27017/trivial-app');
-	const db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/trivial-app',
+	const db = mongoose.connect(process.env.MONGODB_URI,
 		{
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
